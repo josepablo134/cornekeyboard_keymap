20d1fbe391d9850d6046f3d4bd8865c89ffdaa6f
Josepablo C <jcruzbaas@gmail.com>
2026-07-30 21:33:11 -0600

    fix(init.sh): Fix relative path on creation of symblink

--- init.sh
+++ init.sh
@@ -4,7 +4,11 @@ KEYMAP_NAME="josepablocb"
 KEYMAP_HOME="$(pwd)"
 
 if [[ -z "$QMK_HOME" ]]; then
-	QMK_HOME="~/qmk_firmware"
+	QMK_HOME=$(eval echo "~/qmk_firmware")
 fi
 
-ln -s -T $KEYMAP_HOME/$KEYMAP_NAME $QMK_HOME/keyboards/crkbd/keymaps/$KEYMAP_NAME
+echo QMK_HOME=${QMK_HOME}
+echo KEYMAP_NAME=${KEYMAP_NAME}
+echo KEYMAP_HOME=${KEYMAP_HOME}
+
+ln -s ${KEYMAP_HOME}/${KEYMAP_NAME} ${QMK_HOME}/keyboards/crkbd/keymaps/${KEYMAP_NAME}
